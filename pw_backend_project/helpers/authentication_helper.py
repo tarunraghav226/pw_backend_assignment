a767086f4258b2c40433e808b86b1a88b5dd6a7d
tarunraghav226 <tarun.raghav226@gmail.com>
2022-04-08 14:36:36 +0530

    Updated authentication helper file

--- pw_backend_project/helpers/authentication_helper.py
+++ pw_backend_project/helpers/authentication_helper.py
@@ -3,8 +3,8 @@ import bcrypt
 from pw_backend_project.models import CustomUser
 
 class AuthenticationHelper:
-    def get_hashed_password(password: str):
-        return bcrypt.hashpw(password, bcrypt.gensalt())
+    def get_hashed_password(self, password: str):
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-    def authenticate(user: CustomUser, password: str):
-        return bcrypt.checkpw(password, user.passowrd)
+    def authenticate(self, user: CustomUser, password: str):
+        return bcrypt.checkpw(password.encode('utf-8'), user.passowrd)
